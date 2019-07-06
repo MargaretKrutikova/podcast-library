ca3eaf04ce48154db06fc9c3cefeea60a878533d
Margarita Krutikova <margaretkru@gmail.com>
2019-07-07 01:19:43 +0200

    Install dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const outputDir = path.join(__dirname, 'build/');
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const outputDir = path.join(__dirname, "build/")
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production"
+const Dotenv = require("dotenv-webpack")
 
 module.exports = {
-  entry: './src/Index.bs.js',
-  mode: isProd ? 'production' : 'development',
+  entry: "./src/Index.bs.js",
+  mode: isProd ? "production" : "development",
   output: {
     path: outputDir,
-    filename: 'Index.js'
+    filename: "Index.js"
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
+      template: "src/index.html",
       inject: false
-    })
+    }),
+    new Dotenv()
   ],
   devServer: {
     compress: true,
@@ -23,4 +25,4 @@ module.exports = {
     port: process.env.PORT || 8000,
     historyApiFallback: true
   }
-};
+}
